b7147bc4b2c4322985f3318b6d2f88ff28e60890
neby55 <ben@progweb.fr>
2017-04-07 15:14:30 +0200

    Delete flash message after a "show" (#7)
    To be sure a flash message can be displayed only once

--- W/Controller/Controller.php
+++ W/Controller/Controller.php
@@ -116,6 +116,11 @@ class Controller
 
 		// Affiche le template
 		echo $engine->render($file, $data);
+		
+		// Supprime les messages Flash pour qu'ils n'apparaissent qu'une fois
+		if (isset($_SESSION['flash']) {
+			unset($_SESSION['flash']);
+		}
 		die();
 	}
 
